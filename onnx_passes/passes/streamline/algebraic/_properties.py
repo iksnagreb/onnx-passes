512a33314bc166430effde382f1a388b516b492a
Christoph Berganski <christoph.berganski@gmail.com>
2025-07-04 19:04:11 +0200

    Sketch _Commutative and _Associative property transformation templates
    Note: These are not used yet

--- onnx_passes/passes/streamline/algebraic/_properties.py
+++ onnx_passes/passes/streamline/algebraic/_properties.py
@@ -4,6 +4,10 @@ from onnx_passes.passes.base import Transformation, RewriteRulePass
 # Checking ir.Value for being constants and comparing constants to be identical
 from onnx_passes.passes.util import identical_constants, is_constant, is_signed
 
+# Some templates do not fully implement the Transformation or RewriteRulePass
+# methods and need to be tagged as ABC
+import abc
+
 
 # Left-distributivity template: x * (y + z) = x * y + x * z
 class _DistributiveLhs(Transformation, RewriteRulePass):
@@ -41,6 +45,7 @@ class _Distributive(_DistributiveLhs):
     @property
     def commute(self):
         return True
+
 
 # @passes.verify.tolerance
 # @passes.register("algebraic")
@@ -103,3 +108,85 @@ class _Distributive(_DistributiveLhs):
 # class MoveAddPastMatMulRhs(_DistributiveRhs):
 #     __MUL__ = lambda _, op, x, y: op.MatMul(x, y)
 #     __ADD__ = lambda _, op, x, y: op.Add(x, y)
+
+
+# Commutativity template: x + y = y + x
+class _Commutative(Transformation, RewriteRulePass, abc.ABC):
+    @property
+    def commute(self):
+        return True
+
+
+# Associativity template: (x + y) + z = x + (y + z)
+class _Associative(Transformation, RewriteRulePass):
+    __OP__: callable
+
+    def pattern(self, op, x, y, z):
+        return self.__OP__(op, self.__OP__(op, x, y), z)
+
+    def check(self, op, x, y, z):
+        # 1. Group two constants if there is one non-constant input
+        if not is_constant(x) and is_constant(y) and is_constant(z):
+            return True
+        # 2. Group two non-constants if there is one constant input
+        if is_constant(x) and not is_constant(y) and not is_constant(z):
+            return True
+        # 3. Do not change the grouping of all constant or all non-constant
+        return False
+
+    def rewrite(self, op, x, y, z):
+        return self.__OP__(op, x, self.__OP__(op, y, z))
+
+
+# @passes.verify.tolerance
+# @passes.register("algebraic")
+# class GroupAdd(_Associative, _Commutative):
+#     __OP__ = lambda _, op, x, y: op.Add(x, y)
+#
+#
+# @passes.verify.tolerance
+# @passes.register("algebraic")
+# class GroupMul(_Associative, _Commutative):
+#     __OP__ = lambda _, op, x, y: op.Mul(x, y)
+#
+#
+# @passes.verify.tolerance
+# @passes.register("algebraic")
+# class GroupBitwiseOr(_Associative, _Commutative):
+#     __OP__ = lambda _, op, x, y: op.BitwiseOr(x, y)
+#
+#
+# @passes.verify.tolerance
+# @passes.register("algebraic")
+# class GroupBitwiseAnd(_Associative, _Commutative):
+#     __OP__ = lambda _, op, x, y: op.BitwiseAnd(x, y)
+#
+#
+# @passes.verify.tolerance
+# @passes.register("algebraic")
+# class GroupOr(_Associative, _Commutative):
+#     __OP__ = lambda _, op, x, y: op.Or(x, y)
+#
+#
+# @passes.verify.tolerance
+# @passes.register("algebraic")
+# class GroupAnd(_Associative, _Commutative):
+#     __OP__ = lambda _, op, x, y: op.And(x, y)
+#
+#
+# @passes.verify.tolerance
+# @passes.register("algebraic")
+# class GroupMax(_Associative, _Commutative):
+#     __OP__ = lambda _, op, x, y: op.Max(x, y)
+#
+#
+# @passes.verify.tolerance
+# @passes.register("algebraic")
+# class GroupMin(_Associative, _Commutative):
+#     __OP__ = lambda _, op, x, y: op.Min(x, y)
+#
+#
+# @passes.verify.tolerance
+# @passes.register("algebraic")
+# class GroupMatMul(_Associative):
+#     __OP__ = lambda _, op, x, y: op.MatMul(x, y)
