42513754a63e9314611c754a42840a06806d0962
Christoph Berganski <christoph.berganski@gmail.com>
2025-10-17 19:48:02 +0200

    Add QONNX MultiThreshold operator import pass

--- onnx_passes/passes/imports/qonnx.py
+++ onnx_passes/passes/imports/qonnx.py
@@ -11,6 +11,10 @@ from onnx_passes.ops import DOMAIN as CUSTOM_DOMAIN, inject_custom_ops
 # Domain used by QONNX operators which are to be transplanted into CUSTOM_DOMAIN
 from onnx_passes.ops.qonnx import DOMAIN as QONNX_DOMAIN, BREVITAS_DOMAIN
 
+# Utilities to check properties of values and collecting attributes from ONNX
+# nodes and generating matching tensors of ones
+from onnx_passes.passes.util import collect_attrs, is_constant, ones_like
+
 
 # Imports QONNX Quant custom operator nodes from the QONNX domain into the
 # CUSTOM_DOMAIN to enable ONNX Runtime execution
@@ -45,7 +49,66 @@ class ImportBrevitasQuant(Transformation, RewriteRulePass):
         )
 
 
-# TODO: Import BipolarQuant, Trunc and MultiThreshold from the QONNX domain...
+# Imports QONNX MultiThreshold custom operator from the QONNX domain into the
+# CUSTOM_DOMAIN to enable ONNX Runtime execution
+class ImportQONNXMultiThreshold(Transformation, RewriteRulePass):
+    def pattern(self, op, x, thresholds):
+        return op.MultiThreshold(
+            x, thresholds, _domain=QONNX_DOMAIN, _outputs=["y"]
+        )
+
+    def check(self, op, x, thresholds, y):
+        if x.shape is not None and x.shape.is_static():
+            return is_constant(thresholds)
+        return False
+
+    def rewrite(self, op, x, thresholds, y):
+        # Default attributes of the MultiThreshold operator according to QONNX
+        # reference
+        attributes = {
+            "out_dtype": (ir.AttributeType.STRING, ""),
+            "out_scale": (ir.AttributeType.FLOAT, 1.0),
+            "out_bias": (ir.AttributeType.FLOAT, 0.0),
+            "data_layout": (ir.AttributeType.STRING, "NCHW"),
+        }
+
+        # Collect node attributes falling back to defaults defined above
+        attributes = collect_attrs(y.producer(), attributes)
+
+        # Find the channel dimension (see data layout concept in QONNX...)
+        cdim = attributes["data_layout"].as_string().index("C")
+
+        # The thresholds tensor needs to be expanded to explicitly match the
+        # dimensions of the input
+        shape = [*(len(x.shape) * [1]), thresholds.shape[-1]]
+        shape[cdim] = x.shape[cdim]
+
+        # Expand the thresholds input to make all dimensions explicit so we can
+        # get rid of tracking layout information
+        thresholds = op.Reshape(
+            op.Expand(
+                thresholds,
+                op.Constant(value_ints=[x.shape[cdim], thresholds.shape[-1]])
+            ),
+            op.Constant(value_ints=shape)
+        )
+
+        # QONNX MultiThresholds are always monotonically increasing, i.e.,
+        # positive unit steps
+        weights = ones_like(op, thresholds)
+
+        # Replacement pattern with explicit weights, scale and bias, inserted
+        # into our custom domain
+        return op.Add(
+            op.Mul(
+                op.Constant(value_float=attributes["out_scale"].as_float()),
+                op.MultiThreshold(x, thresholds, weights, _domain=CUSTOM_DOMAIN)
+            ),
+            op.Constant(value_float=attributes["out_bias"].as_float())
+        )
+
+
+# TODO: Import BipolarQuant and Trunc from the QONNX domain...
 
 
 # ir.Model, ir.passes.PassResult, ir.from_proto, ir.to_proto, ...
@@ -85,5 +148,8 @@ class _ConvertQONNXMinimumVersion(passes.base.Annotation):
 @passes.register("import-qonnx")
 class ImportQONNX(passes.compose.ComposePass, passes.base.Transformation):
     __passes__ = [
-        _ConvertQONNXMinimumVersion, ImportQONNXQuant, ImportBrevitasQuant
+        _ConvertQONNXMinimumVersion,
+        ImportQONNXQuant,
+        ImportBrevitasQuant,
+        ImportQONNXMultiThreshold
     ]
