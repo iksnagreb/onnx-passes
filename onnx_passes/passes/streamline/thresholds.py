eb02fa1a5ee95928089f16f0871ebbbf317fa441
Christoph Berganski <christoph.berganski@gmail.com>
2026-01-03 15:37:18 +0100

    Add threshold sorting and monotonicity and multiplicity decomposition

--- onnx_passes/passes/streamline/thresholds.py
+++ onnx_passes/passes/streamline/thresholds.py
@@ -66,6 +66,9 @@ class StreamlineThresholds(ComposePass, Transformation):
         # Convert back to MultiThreshold custom-ops representing the
         # thresholding operations
         "fuse-thresholds",
+        # Bring all thresholds into ascending order as the normal form required
+        # for efficient implementation via binary search
+        "sort-thresholds",
         # After rearranging the graph, make sure everything is properly
         # annotated with shapes
         "shape-inference",
@@ -191,12 +194,117 @@ class ConvertRoundToThresholds(Transformation, RewriteRuleSetPass):
         ]
 
 
-# Threshold decomposition involves approximate unbroadcasting of replacement
-# thresholds at ist core
+def _sort_thresholds(thresholds: np.ndarray, weights: np.ndarray):
+    return (
+        np.take_along_axis(thresholds, np.argsort(thresholds), axis=-1),
+        np.take_along_axis(weights, np.argsort(thresholds), axis=-1),
+    )
+
+
+@passes.verify.tolerance
+@passes.register("sort-thresholds")
+class SortThresholds(Transformation, RewriteRulePass):
+    def pattern(self, op, x, thresholds, weights):
+        return op.MultiThreshold(x, thresholds, weights, _domain=CUSTOM_DOMAIN)
+
+    def check(self, op, x, thresholds, weights):
+        # Thresholds must be constant to be decomposed, otherwise there are no
+        # known values to extract and iterate
+        if (thresholds := ir.convenience.get_const_tensor(thresholds)) is None:
+            return False
+
+        # Weights must be constant to be decomposed, otherwise there are no
+        # known values to extract and iterate
+        if (weights := ir.convenience.get_const_tensor(weights)) is None:
+            return False
+
+        # Do not sort again if thresholds are already sorted
+        return np.any(np.sort(thresholds.numpy(), axis=-1) != thresholds)
+
+    def rewrite(self, op, x, thresholds, weights):
+        # Sort thresholds and associated step direction weights in numpy format
+        thresholds, weights = _sort_thresholds(
+            ir.convenience.get_const_tensor(thresholds).numpy(),
+            ir.convenience.get_const_tensor(weights).numpy(),
+        )
+
+        # Insert thresholds back into ONNX constants and make sure the type is
+        # the same as the input as required by ONNX standard of elementwise
+        thresholds = op.Constant(value=ir.tensor(thresholds))
+        weights = op.Constant(value=ir.tensor(weights))
+
+        # Replacement pattern: MultiThreshold operator with thresholds and
+        # weights sorted into ascending order of thresholds
+        return op.MultiThreshold(x, thresholds, weights, _domain=CUSTOM_DOMAIN)
+
+
+def _decompose_monotonicity(thresholds: np.ndarray, weights: np.ndarray):
+    # Get rid of all zero steps by setting the corresponding threshold to
+    # infinity, i.e., x > inf = False
+    thresholds = np.where(weights == 0.0, np.inf, thresholds)
+
+    # Decreasing (< 0.0) and increasing (> 0.0) segments of the thresholds, no
+    # need to return the weights as these are always abs(weights)
+    return (
+        np.where(weights < 0.0, thresholds, np.inf),
+        np.where(weights > 0.0, thresholds, np.inf)
+    )
+
+
+# Decomposes non-monotonic (i.e., increasing and decreasing) thresholds into the
+# difference of two increasing multi-threshold functions
+@passes.verify.tolerance
+@passes.register("decompose-thresholds")
+class ThresholdMonotonicityDecomposition(Transformation, RewriteRulePass):
+    def pattern(self, op, x, thresholds, weights):
+        return op.MultiThreshold(x, thresholds, weights, _domain=CUSTOM_DOMAIN)
+
+    def check(self, op, x, thresholds, weights):
+        # Thresholds must be constant to be decomposed, otherwise there are no
+        # known values to extract and iterate
+        if (thresholds := ir.convenience.get_const_tensor(thresholds)) is None:
+            return False
+
+        # Weights must be constant to be decomposed, otherwise there are no
+        # known values to extract and iterate
+        if (weights := ir.convenience.get_const_tensor(weights)) is None:
+            return False
+
+        # Decompose if there are increasing (positive) and decreasing (negative
+        # weights)
+        return np.any(weights.numpy() <= 0)
+
+    def rewrite(self, op, x, thresholds, weights):
+        # We start with the initial thresholds and weights in numpy format
+        _thresholds = ir.convenience.get_const_tensor(thresholds).numpy()
+        _weights = ir.convenience.get_const_tensor(weights).numpy()
+
+        # Decompose the threshold tensor into increasing and decreasing segments
+        decreasing, increasing = _decompose_monotonicity(_thresholds, _weights)
+
+        # Insert thresholds back into ONNX constants and make sure the type is
+        # the same as the input as required by ONNX standard of elementwise
+        increasing = op.CastLike(op.Constant(value=ir.tensor(increasing)), x)
+        decreasing = op.CastLike(op.Constant(value=ir.tensor(decreasing)), x)
+
+        # Replace weights by positive steps with the same magnitude (opposite
+        # direction steps are skipped by mapping to infinity)
+        weights = op.Abs(weights)
+
+        # Replacement pattern: MultiThreshold decomposed into a difference of
+        # two monotonically increasing MultiThreshold functions
+        return op.Sub(
+            op.MultiThreshold(x, increasing, weights, _domain=CUSTOM_DOMAIN),
+            op.MultiThreshold(x, decreasing, weights, _domain=CUSTOM_DOMAIN)
+        )
+
+
+# Threshold granularity decomposition involves approximate unbroadcasting of
+# replacement thresholds at ist core
 from onnx_passes.passes.util import unbroadcast
 
 
-def _decompose_thresholds(thresholds: np.ndarray):
+def _decompose_granularity(thresholds: np.ndarray):
     # Draw some random thresholds covering only the final axis to replace
     # the fine-granular thresholds. Thresholds should always be sorted.
     t = np.sort(np.random.rand(*thresholds.shape[-2:]), axis=-1)
@@ -212,7 +320,7 @@ def _decompose_thresholds(thresholds: np.ndarray):
 # integer-valued elementwise addition and per-channel thresholds if possible.
 @passes.verify.tolerance
 @passes.register("decompose-thresholds")
-class DecomposeThresholds(Transformation, RewriteRulePass):
+class ThresholdGranularityDecomposition(Transformation, RewriteRulePass):
     def pattern(self, op, x, thresholds, weights):
         return op.MultiThreshold(x, thresholds, weights, _domain=CUSTOM_DOMAIN)
 
@@ -229,7 +337,7 @@ class DecomposeThresholds(Transformation, RewriteRulePass):
 
         # Decompose the threshold tensor into new per-channel thresholds and a
         # corresponding per-element bias
-        t, bias = _decompose_thresholds(thresholds.numpy())
+        t, bias = _decompose_granularity(thresholds.numpy())
 
         # The threshold decomposition must actually be at most per-channel
         # granularity. No need to verify the bias shape, this is allowed to be a
@@ -242,12 +350,11 @@ class DecomposeThresholds(Transformation, RewriteRulePass):
         return True
 
     def rewrite(self, op, x, thresholds, weights):
-        # We start with the initial fine-granular thresholds in numpy format
-        thresholds = ir.convenience.get_const_tensor(thresholds).numpy()
-
         # Decompose the threshold tensor into new per-channel thresholds and a
         # corresponding per-element bias
-        thresholds, bias = _decompose_thresholds(thresholds)
+        thresholds, bias = _decompose_granularity(
+            ir.convenience.get_const_tensor(thresholds).numpy()
+        )
 
         # If the step weights are constant, try unbroadcasting as an unrelated
         # optimization along the way
@@ -269,3 +376,90 @@ class DecomposeThresholds(Transformation, RewriteRulePass):
         return op.MultiThreshold(
             op.Add(x, bias), thresholds, weights, _domain=CUSTOM_DOMAIN
         )
+
+
+def _decompose_multiplicity(thresholds: np.ndarray, weights: np.ndarray):
+    # Remember original shape of leading dimensions before flattening the arrays
+    # to simplify the padding and iteration below
+    thresholds_shape = thresholds.shape[:-1]
+    weights_shape = weights.shape[:-1]
+
+    thresholds = thresholds.reshape((-1, thresholds.shape[-1]))
+    weights = weights.reshape((-1, weights.shape[-1]))
+
+    # Get rid of all "dead" thresholds by setting their multiplicity to zero
+    weights = np.where(thresholds >= np.inf, 0, weights)
+
+    # Number of steps per set of thresholds and maximum number of steps of the
+    # entire set
+    n = np.sum(np.abs(weights), axis=-1, keepdims=True)
+    max_n = np.max(np.sum(np.abs(weights), axis=-1, keepdims=True))
+
+    # Add infinity padding to the threshold list, pad each set of thresholds by
+    # the amount necessary to have the same number of thresholds for each set
+    thresholds = np.pad(thresholds, ((0, 0), (0, 1)), constant_values=np.inf)
+    weights = np.pad(weights, ((0, 0), (0, 1)), constant_values=max_n - n)
+
+    # Number of repetitions of each threshold, i.e., integer-valued threshold
+    # multiplicity
+    repeats = np.abs(weights).astype(np.int64)
+
+    # Repeat all thresholds according to their multiplicity, i.e., magnitude of
+    # the associated step direction weight and map weights to +/- unit steps
+    thresholds = [np.repeat(ts, num) for ts, num in zip(thresholds, repeats)]
+    weights = [np.repeat(np.sign(ws), num) for ws, num in zip(weights, repeats)]
+
+    # Restore original shape of the leading dimensions of the threshold and
+    # weight tensor
+    return (
+        np.reshape(np.asarray(thresholds), shape=(*thresholds_shape, -1)),
+        np.reshape(np.asarray(weights), shape=(*weights_shape, -1))
+    )
+
+
+# Decomposes non-unit step thresholds by replicating steps according to their
+# multiplicity if possible (all weights are integers).
+@passes.verify.tolerance
+@passes.register("decompose-thresholds")
+class ThresholdMultiplicityDecomposition(Transformation, RewriteRulePass):
+    def pattern(self, op, x, thresholds, weights):
+        return op.MultiThreshold(x, thresholds, weights, _domain=CUSTOM_DOMAIN)
+
+    def check(self, op, x, thresholds, weights):
+        # Thresholds must be constant to be decomposed, otherwise there are no
+        # known values to extract and iterate
+        if (thresholds := ir.convenience.get_const_tensor(thresholds)) is None:
+            return False
+
+        # Weights must be constant to be decomposed, otherwise there are no
+        # known values to extract and iterate
+        if (weights := ir.convenience.get_const_tensor(weights)) is None:
+            return False
+
+        # Convert ONNX tensor to NumPy array for further checks
+        weights, thresholds = weights.numpy(), thresholds.numpy()
+
+        # All weights must be integers to apply the decomposition, as we cannot
+        # fractionally replicate a step
+        if np.any(np.asarray(np.round(weights), dtype=np.int64) != weights):
+            return False
+
+        # Decompose if there are weights that are not +/- 1
+        return np.any(np.abs(weights) != 1.0) or np.any(thresholds >= np.inf)
+
+    def rewrite(self, op, x, thresholds, weights):
+        # Apply the decomposition to thresholds and weights converted to NumPy
+        # format
+        thresholds, weights = _decompose_multiplicity(
+            ir.convenience.get_const_tensor(thresholds).numpy(),
+            ir.convenience.get_const_tensor(weights).numpy()
+        )
+
+        # Insert thresholds back into ONNX constants and make sure the type is
+        # the same as the input as required by ONNX standard of elementwise
+        thresholds = op.Constant(value=ir.tensor(thresholds))
+        weights = op.Constant(value=ir.tensor(weights))
+
+        # Replacement pattern: MultiThreshold operator with thresholds and
+        # weights repeated according to their multiplicity
+        return op.MultiThreshold(x, thresholds, weights, _domain=CUSTOM_DOMAIN)
