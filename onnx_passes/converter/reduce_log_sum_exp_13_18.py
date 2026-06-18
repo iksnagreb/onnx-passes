1d64f5df333cb7ca0270e24e97d2748ab6fa4b51
Linus Jungemann <38974033+LinusJungemann@users.noreply.github.com>
2026-06-18 15:13:08 +0200

    Enable version conversion

--- onnx_passes/converter/reduce_log_sum_exp_13_18.py
+++ onnx_passes/converter/reduce_log_sum_exp_13_18.py
@@ -1,28 +1,33 @@
 # Collecting node attributes with optional defaults
+from collections.abc import Sequence
+
 from onnx_passes.passes.util import collect_attrs
 
 from onnxscript.version_converter._version_converter import registry
 from onnxscript import ir
+import onnxscript.ir._tape as _tape
+
+RewriterContext = _tape.Builder
+ReturnValue = Sequence[ir.Value] | ir.Value | None
 
 register = registry.register
 
 
 # Onnx 13 -> 18
-@register("ReduceLogSumExp", node_version=13, up_conversion=True)
-def convert_reduce_log_sum_exp_13_18(node: ir.Node, op):
-    input = node.inputs[0]
+@register("ReduceLogSumExp", node_version=18, up_conversion=True)
+def convert_reduce_log_sum_exp_13_18(node: ir.Node, op: RewriterContext) -> ReturnValue:
+    inp = node.inputs[0]
 
     attributes = collect_attrs(
         node,
         {
-            "keepdims": (ir.AttributeType.INT),
+            "keepdims": (ir.AttributeType.INT, 1),
             "axes": (ir.AttributeType.INTS, ()),
         },
     )
     kwargs = {
-                "keepdims": attributes["keepdims"].as_int(),
-                "noop_with_empty_axes": 0,
-                "axes": attributes["axes"].as_ints(),
-            }
-    
-    return op.ReduceLogSumExp(input, **kwargs)
+        "keepdims": attributes["keepdims"].as_int(),
+        "noop_with_empty_axes": 0,
+    }
+
+    return op.ReduceLogSumExp(inp, op.Constant(value_ints=attributes["axes"].as_ints()), **kwargs)
