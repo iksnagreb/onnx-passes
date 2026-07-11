ead615dfe2c7c9d1be4f8be0202d6ab243503998
Christoph Berganski <christoph.berganski@gmail.com>
2026-07-11 15:14:02 +0200

    Verify after dedicated Cleanup pass

--- onnx_passes/passes/_cleanup/__init__.py
+++ onnx_passes/passes/_cleanup/__init__.py
@@ -1,9 +1,10 @@
 from onnx_passes.passes._base import Transformation
+from onnx_passes.passes._verify import Verify
 
 import onnx_ir as ir
 
 
-class Cleanup_v1(Transformation):
+class Cleanup_v1(Transformation, Verify):
     """Basic cleanup of the model without transforming anything."""
 
     def call(self, model: ir.Model) -> ir.passes.PassResult:
