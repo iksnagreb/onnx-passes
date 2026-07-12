122683764f91166e23c16c46583adababa6437d8
Christoph Berganski <christoph.berganski@gmail.com>
2026-07-12 18:20:01 +0200

    Recreate Where branch elimination pass

--- onnx_passes/passes/_eliminate/__init__.py
+++ onnx_passes/passes/_eliminate/__init__.py
@@ -1,13 +1,15 @@
 from onnx_passes.passes._base import Transformation, Sequential
 
-from onnx_passes.passes._eliminate.cse import CommonSubexpressionElimination_v1
+from onnx_passes.passes._eliminate import cse
+from onnx_passes.passes._eliminate import branches
 
 
 class Eliminate_v1(Sequential, Transformation):
     """Exhaustively applies common eliminations transformations."""
 
     passes = [
-        CommonSubexpressionElimination_v1,
+        cse,
+        branches
     ]
 
     exhaustive = True
