a7015aced0556a37d9ac29f6839260b84932873f
Christoph Berganski <christoph.berganski@gmail.com>
2026-07-12 17:18:40 +0200

    Add cleanup pass to canonicalize pass sequence

--- onnx_passes/passes/_canonicalize/__init__.py
+++ onnx_passes/passes/_canonicalize/__init__.py
@@ -1,10 +1,10 @@
 from onnx_passes.passes._base import Sequential, Transformation
 
-from onnx_passes.passes import _inline, _normalize
+from onnx_passes.passes import _inline, _normalize, _cleanup
 
 
 class Canonicalize_v1(Sequential, Transformation):
     """Canonicalize the graph: Combines inlining and normalization passes."""
     passes = [
-        _inline, _normalize
+        _cleanup, _inline, _normalize
     ]
