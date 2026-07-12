c2723ae0a4c08e256628c08a2a50da5743505314
Christoph Berganski <christoph.berganski@gmail.com>
2026-07-12 22:46:57 +0200

    Recreate most identity elimination rules

--- onnx_passes/passes/_eliminate/__init__.py
+++ onnx_passes/passes/_eliminate/__init__.py
@@ -2,6 +2,10 @@ from onnx_passes.passes._base import Transformation, Sequential
 
 from onnx_passes.passes._eliminate import cse
 from onnx_passes.passes._eliminate import branches
+from onnx_passes.passes._eliminate import identities
+
+from onnx_passes.passes import _normalize
+from onnx_passes.passes import _fold_constants
 
 
 class Eliminate_v1(Sequential, Transformation):
@@ -9,7 +13,10 @@ class Eliminate_v1(Sequential, Transformation):
 
     passes = [
         cse,
-        branches
+        branches,
+        identities,
+        _fold_constants,
+        _normalize,
     ]
 
     exhaustive = True
