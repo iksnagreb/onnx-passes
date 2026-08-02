d3c1e766ec7c2b8fd4e3ed8e449d56589ca6861c
Christoph Berganski <christoph.berganski@gmail.com>
2026-08-02 17:32:55 +0200

    Add MoveMatMulPastTranspose_v1 reordering pass

--- onnx_passes/passes/_reorder/transpose.py
+++ onnx_passes/passes/_reorder/transpose.py
@@ -1,7 +1,10 @@
 from onnx_passes.passes._base import RewriteRule
-from onnx_passes.passes._verify import Verify
+from onnx_passes.passes._verify import Verify, tolerance
 
 from onnx_passes.traits.elementwise import produced_by_elementwise
+
+import numpy as np
+import onnx_ir as ir
 
 
 class MoveElementwisePastTranspose_v1(RewriteRule, Verify):
@@ -38,3 +41,170 @@ class MoveElementwisePastTranspose_v1(RewriteRule, Verify):
         # Insert the replacement pattern with attributes transplanted from the
         # elementwise operator
         return op.op(elementwise.op_type, *inputs, **elementwise.attributes)
+
+
+@tolerance
+class MoveMatMulPastTranspose_v1(RewriteRule, Verify):
+    """Reorder matrix multiplication to follow transpose where applicable."""
+
+    @staticmethod
+    def pattern(op, x, y, perm):
+        return op.Transpose(op.MatMul(x, y), perm=perm)
+
+    @staticmethod
+    def check(op, x, y, perm):
+        if x.shape is not None and x.shape.is_static():
+            if y.shape is not None and y.shape.is_static():
+                perm = np.array(perm.as_ints()) - np.int64(len(perm.as_ints()))
+                return tuple(perm[-2:]) in {(-1, -2), (-2, -1)}
+        return False
+
+    @staticmethod
+    def rewrite(op, x, y, perm):
+        # Permutation as numpy array with reverse indexing (simplifies checks
+        # for swapping the final two axes)
+        perm = np.array(perm.as_ints()) - np.int64(len(perm.as_ints()))
+
+        # The replacement pattern is decided by the shape signature of the
+        # inputs roughly following the broadcasting behavior of the matmul
+        signature = (len(x.shape), len(y.shape))
+
+        # 2d - 2d: Transpose both sides using the same 2d permutation
+        if signature == (2, 2):
+            x = op.Transpose(x, perm=[int(i) + len(perm) for i in perm])
+            y = op.Transpose(y, perm=[int(i) + len(perm) for i in perm])
+
+        # 1d - 2d: Output is 1d, nothing is transposed
+        # TODO: Remove this, we should never end up here...
+        if signature == (1, 2):
+            x = x
+            y = y
+
+        # 2d - 1d: Output is 1d, nothing is transposed
+        # TODO: Remove this, we should never end up here...
+        if signature == (2, 1):
+            x = x
+            y = y
+
+        # 1d - Nd: Transpose right hand side with added permutation axis as this
+        # product yields (N-1)d at outputs.
+        if signature[0] == 1 and signature[1] > 2:
+            x = x
+
+            # Swap the final two axes (one more axis than the output) first, if
+            # the output permutes those axes
+            if tuple(perm[-2:]) == (-1, -2):
+                y = op.Transpose(
+                    y, perm=[*range(len(perm) - 1), len(perm), len(perm) - 1]
+                )
+
+            y = op.Transpose(
+                y, perm=[*[int(i) + len(perm) for i in perm], len(perm)]
+            )
+
+        # Nd - 1d: Transpose left hand side with added permutation axis as this
+        # product yields (N-1)d at outputs.
+        if signature[0] > 2 and signature[1] == 1:
+            x = op.Transpose(
+                x, perm=[*[int(i) + len(perm) for i in perm], len(perm)]
+            )
+
+            # Swap the final two axes (one more axis than the output) last, if
+            # the output permutes those axes
+            if tuple(perm[-2:]) == (-1, -2):
+                x = op.Transpose(
+                    x, perm=[*range(len(perm) - 1), len(perm), len(perm) - 1]
+                )
+
+            y = y
+
+        # 2d - Nd: Transpose the right hand side where the output permutation
+        # directly applies. If the last two axes are swapped, also permute the
+        # left hand side.
+        if signature[0] == 2 and signature[1] > 2:
+            if tuple(perm[-2:]) == (-1, -2):
+                x = op.Transpose(x)
+
+            y = op.Transpose(y, perm=[int(i) + len(perm) for i in perm])
+
+        # Nd - 2d: Transpose the left hand side where the output permutation
+        # directly applies. If the last two axes are swapped, also permute the
+        # right hand side.
+        if signature[0] > 2 and signature[1] == 2:
+            if tuple(perm[-2:]) == (-1, -2):
+                y = op.Transpose(y)
+
+            x = op.Transpose(x, perm=[int(i) + len(perm) for i in perm])
+
+        # Nd - Md: Transpose both inputs after unsqueezing the shorter to match
+        # the rank of the longer. Squeeze leading dimensions after transpose.
+        if signature[0] > 2 and signature[1] > 2:
+            unsqueezed_x = ...
+            unsqueezed_y = ...
+
+            if signature[0] < signature[1]:
+                x = op.Unsqueeze(
+                    x,
+                    unsqueezed_x := op.Constant(
+                        value_ints=list(range(signature[1] - signature[0]))
+                    )
+                )
+
+            if signature[1] < signature[0]:
+                y = op.Unsqueeze(
+                    y,
+                    unsqueezed_y := op.Constant(
+                        value_ints=list(range(signature[0] - signature[1]))
+                    )
+                )
+
+            x = op.Transpose(x, perm=[int(i) + len(perm) for i in perm])
+            y = op.Transpose(y, perm=[int(i) + len(perm) for i in perm])
+
+            if signature[0] < signature[1]:
+                x = op.Squeeze(  # noqa: Duplicate, see below...
+                    x,
+                    op.Range(
+                        op.Constant(value_int=0),
+                        op.Min(
+                            op.ArgMin(
+                                op.Cast(
+                                    op.Equal(
+                                        op.Shape(x),
+                                        op.Constant(value_int=1)
+                                    ),
+                                    to=ir.DataType.UINT8
+                                )
+                            ),
+                            op.Size(unsqueezed_x)
+                        ),
+                        op.Constant(value_int=1),
+                    )
+                )
+            if signature[1] < signature[0]:
+                y = op.Squeeze(  # noqa: Duplicate, see below...
+                    y,
+                    op.Range(
+                        op.Constant(value_int=0),
+                        op.Min(
+                            op.ArgMin(
+                                op.Cast(
+                                    op.Equal(
+                                        op.Shape(y),
+                                        op.Constant(value_int=1)
+                                    ),
+                                    to=ir.DataType.UINT8
+                                )
+                            ),
+                            op.Size(unsqueezed_y)
+                        ),
+                        op.Constant(value_int=1),
+                    )
+                )
+
+        # Swap the order of inputs if permuting the final two axes as
+        #   (x @ y)^T = y^T @ x^T for 2d matrices
+        if tuple(perm[-2:]) == (-1, -2):
+            return op.MatMul(y, x)
+
+        return op.MatMul(x, y)
