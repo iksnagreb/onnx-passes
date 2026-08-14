8e58f27b45b68420eece50dad90de0f55ddddd18
Christoph Berganski <christoph.berganski@gmail.com>
2026-08-14 14:34:55 +0200

    Add RewriteGreaterAsGreaterOrEqual_v1 normalization pass

--- onnx_passes/passes/_normalize/comparison.py
+++ onnx_passes/passes/_normalize/comparison.py
@@ -1,5 +1,8 @@
-from onnx_passes.passes._base import RewriteRuleSet
+from onnx_passes.passes._base import RewriteRule, RewriteRuleSet
 from onnx_passes.passes._verify import Verify
+
+import onnx_ir as ir
+import numpy as np
 
 
 class RewriteLessAsGreater_v1(RewriteRuleSet, Verify):
@@ -18,6 +21,81 @@ class RewriteLessAsGreater_v1(RewriteRuleSet, Verify):
             lambda op, x, y: op.Greater(y, x),
             lambda op, x, y: op.GreaterOrEqual(y, x),
         ]
+
+
+class RewriteGreaterAsGreaterOrEqual_v1(RewriteRule, Verify):
+    """Rewrite strict as non-strict greater than comparison to constants."""
+
+    @staticmethod
+    def pattern(op, x, c):
+        return op.Greater(x, c)
+
+    @staticmethod
+    def check(context, x, c):
+        return ir.convenience.get_const_tensor(c) is not None
+
+    @staticmethod
+    def rewrite(op, x, c: ir.Value):
+        if c.dtype.is_integer():  # noqa: dtype is never None
+            return op.And(
+                # Non-strict comparison to next larger integer
+                op.GreaterOrEqual(
+                    x,
+                    op.Add(
+                        c,
+                        op.CastLike(
+                            op.Constant(value_int=1),
+                            c
+                        )
+                    )
+                ),
+                # Mask: x > dtype.max is always False
+                op.Not(
+                    op.Equal(
+                        c,
+                        op.CastLike(
+                            op.Constant(value_int=c.dtype.max),  # noqa: dtype
+                            c
+                        )
+                    )
+                )
+            )
+
+        # Sanitized nextafter calculations in NumPy: (1) wrap around infinity
+        # to negative infinity as this is masked to False anyway but x >= -inf
+        # offers potential for constant elimination, (2) avoid avoerflow when
+        # applying nextafter to the maximum non infinity value, and (3) apply
+        # nextafter to all other valid inputs.
+        y = ir.convenience.get_const_tensor(c).numpy().copy()  # noqa: not None
+
+        is_inf = y == np.inf
+        is_max = y == c.dtype.max  # noqa: dtype
+        finite = ~is_inf & ~is_max
+
+        y[is_inf] = np.asarray(-np.inf, dtype=y.dtype)
+        y[is_max] = np.asarray(+np.inf, dtype=y.dtype)  # noqa: dtype
+        y[finite] = np.nextafter(y[finite], np.asarray(np.inf, dtype=y.dtype))
+
+        return op.And(
+            # Non-strict comparison to next larger float
+            op.GreaterOrEqual(
+                x,
+                op.CastLike(
+                    op.Constant(value=ir.tensor(y)),
+                    c
+                )
+            ),
+            # Mask: x > inf is always False
+            op.Not(
+                op.Equal(
+                    c,
+                    op.CastLike(
+                        op.Constant(value_float=np.inf),
+                        c
+                    )
+                )
+            )
+        )
 
 
 class SimplifyCompoundComparison_v1(RewriteRuleSet, Verify):
