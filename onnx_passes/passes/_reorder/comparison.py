a098a31b55f837488f2a7060ddb54863880bf960
Christoph Berganski <christoph.berganski@gmail.com>
2026-08-16 22:29:55 +0200

    Add SortConstantComparison_v2 which also sorts boolean coefficients

--- onnx_passes/passes/_reorder/comparison.py
+++ onnx_passes/passes/_reorder/comparison.py
@@ -58,17 +58,20 @@ class SortConstantComparison_v1(RewriteRuleSetTemplate, Verify):
     def pattern(partial, op, x, y, a, b):
         comparison, connective = partial(op)
 
-        # Match (x <=> a) . (x <=> b), as well as (x <=> a) . ((x <=> b) . ...)
+        # Match two options of the pattern, the base case (1), and the
+        # potentially recursive case (2), continuing to sort inward.
+        #   (1) (x <=> a) .  (x <=> b)
+        #   (2) (x <=> a) . ((x <=> b) . y...)
         return connective(
             comparison(x, a),
             OrValue([
                 comparison(x, b),
                 # Recursive application along associative chains or termination
                 # in some other connective or constant.
-                connective(
-                    comparison(x, b),
-                    y
-                )
+                connective(comparison(x, b), y),
+                # Allow the recursive part (or termination) to commute, pushing
+                # the y chain inward with the replacement pattern.
+                connective(y, comparison(x, b)),
             ])
         )
 
@@ -101,6 +104,173 @@ class SortConstantComparison_v1(RewriteRuleSetTemplate, Verify):
             comparison(
                 x,
                 op.Constant(value=ir.tensor(a))
+            ),
+            rhs
+        )
+
+
+@tolerance
+class SortConstantComparison_v2(RewriteRuleSetTemplate, Verify):
+    """Sort likewise constant comparison over commutative (boolean) connectives.
+
+    For likewise comparison <=> with a common input x and constants c1 and c2
+    and constant boolean coefficients a1 and a2 the constants and coefficients
+    are swapped, such that after sorting:
+
+    a1 >= a2 and a1 == a2 -> c1 <= c2: a1(x >= c1) . a2(x >= c2)
+
+    Note: This also assumes associativity to apply sorting to nested connected
+    comparisons, i.e., to sort a1(x >= c1) . (a2(x >= c2) . (a3(x >= c3) . y)).
+    """
+
+    patterns = (
+        lambda op: (op.GreaterOrEqual, op.Xor),
+        lambda op: (op.Greater, op.Xor),
+        lambda op: (op.Equal, op.Xor),
+        lambda op: (op.Less, op.Xor),
+        lambda op: (op.LessOrEqual, op.Xor),
+
+        lambda op: (op.GreaterOrEqual, op.Or),
+        lambda op: (op.Greater, op.Or),
+        lambda op: (op.Equal, op.Or),
+        lambda op: (op.Less, op.Or),
+        lambda op: (op.LessOrEqual, op.Or),
+
+        lambda op: (op.GreaterOrEqual, op.And),
+        lambda op: (op.Greater, op.And),
+        lambda op: (op.Equal, op.And),
+        lambda op: (op.Less, op.And),
+        lambda op: (op.LessOrEqual, op.And),
+    )
+
+    @staticmethod
+    def pattern(partial, op, x, y, a1, a2, c1, c2):
+        comparison, connective = partial(op)
+
+        # Match two options of the pattern, the base case (1), and the
+        # potentially recursive case (2), continuing to sort inward.
+        #   (1) a1(x <=> c1) .  a2(x <=> c2)
+        #   (2) a1(x <=> c1) . (a2(x <=> c2) . y...)
+        return connective(
+            OrValue([
+                comparison(x, c1),
+                # Allow coefficients to commute but not the connective, as
+                # sorting is with respect to the connective
+                op.And(a1, comparison(x, c1)),
+                op.And(comparison(x, c1), a1)
+            ]),
+            OrValue([
+                rhs := OrValue([
+                    comparison(x, c2),
+                    # Allow coefficients to commute but not the connective, as
+                    # sorting is with respect to the connective
+                    op.And(a2, comparison(x, c2)),
+                    op.And(comparison(x, c2), a2)
+                ]),
+                # Recursive application along associative chains or termination
+                # in some other connective or constant.
+                connective(rhs, y),
+                # Allow the recursive part (or termination) to commute, pushing
+                # the y chain inward with the replacement pattern.
+                connective(y, rhs)
+            ])
+        )
+
+    @staticmethod
+    def check(context, x, y, a1, a2, c1, c2):
+        # Coefficients a1, a2 are optional, if not present they are implicitly
+        # assumed to be True as And(True, x) = x.
+        if a1 is not None:
+            if (a1 := ir.convenience.get_const_tensor(a1)) is not None:
+                a1 = a1.numpy()
+            else:
+                return False
+        else:
+            a1 = np.asarray(True)
+
+        if a2 is not None:
+            if (a2 := ir.convenience.get_const_tensor(a2)) is not None:
+                a2 = a2.numpy()
+            else:
+                return False
+        else:
+            a2 = np.asarray(True)
+
+        # Comparison constants are not optional, both must be resent to decide
+        # whether we need to continue swapping neighboring comparisons.
+        if (c1 := ir.convenience.get_const_tensor(c1)) is None:
+            return False
+
+        if (c2 := ir.convenience.get_const_tensor(c2)) is None:
+            return False
+
+        # Swap neighbors if any coefficients are out of order, or if comparisons
+        # for matching coefficients are out of order.
+        return np.any((a1 < a2) | (a1 == a2) & (c1.numpy() > c2.numpy()))
+
+    @staticmethod
+    def rewrite(partial, op, x, y, a1, a2, c1, c2):
+        comparison, connective = partial(op)
+
+        # Coefficients a1, a2 are optional, if not present they are implicitly
+        # assumed to be True as And(True, x) = x. Comparison constants are not
+        # optional and always present as guaranteed by the match condition.
+        if a1 is not None:
+            if (a1 := ir.convenience.get_const_tensor(a1)) is not None:
+                a1 = a1.numpy()
+            else:
+                a1 = np.asarray(True)
+        else:
+            a1 = np.asarray(True)
+
+        if a2 is not None:
+            if (a2 := ir.convenience.get_const_tensor(a2)) is not None:
+                a2 = a2.numpy()
+            else:
+                a2 = np.asarray(True)
+        else:
+            a2 = np.asarray(True)
+
+        c1 = ir.convenience.get_const_tensor(c1).numpy()  # noqa: not None
+        c2 = ir.convenience.get_const_tensor(c2).numpy()  # noqa: not None
+
+        # This is one step of bubblesort swapping neighboring elements. One pass
+        # is done reaching the base case where y is None. Sorting is done if the
+        # check yields False for all (a1,a2),(c1,c2) pairs along the chain.
+        rhs = op.And(
+            comparison(
+                x,
+                op.Constant(
+                    value=ir.tensor(
+                        np.where(
+                            a1 == a2, np.maximum(c1, c2), np.where(
+                                a1 < a2, c1, c2
+                            )
+                        )
+                    )
+                )
+            ),
+            op.Constant(value=ir.tensor(np.minimum(a1, a2)))
+        )
+
+        if y is not None:
+            rhs = connective(rhs, y)
+
+        return connective(
+            op.And(
+                comparison(
+                    x,
+                    op.Constant(
+                        value=ir.tensor(
+                            np.where(
+                                a1 == a2, np.minimum(c1, c2), np.where(
+                                    a1 < a2, c2, c1
+                                )
+                            )
+                        )
+                    )
+                ),
+                op.Constant(value=ir.tensor(np.maximum(a1, a2)))
             ),
             rhs
         )
@@ -158,8 +328,9 @@ class ReorderComparisonLoop_v1(Sequential, Transformation):
     passes = [
         ReorderTransitiveEqual_v1,
         SortConstantComparison_v1,
+        SortConstantComparison_v2,
         ReorderConstantComparison_v1,
-        ReorderTernaryComparison_v1
+        ReorderTernaryComparison_v1,
     ]
 
     exhaustive = True
