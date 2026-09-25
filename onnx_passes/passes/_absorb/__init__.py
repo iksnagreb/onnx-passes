a01ddeb55bd023335c562e387e1a42e561658bcf
Christoph Berganski <christoph.berganski@gmail.com>
2026-09-25 14:24:14 +0200

    Add Absorb_v1 absorption rules meta pass

---
+++ onnx_passes/passes/_absorb/__init__.py
@@ -0,0 +1,18 @@
+from onnx_passes.passes._base import Transformation, Sequential
+
+from onnx_passes.passes._absorb import arithmetic
+from onnx_passes.passes._absorb import minmax
+
+from onnx_passes.passes import _reorder
+
+
+class Absorb_v1(Sequential, Transformation):
+    """Exhaustively applies common absorption transformations."""
+
+    passes = [
+        arithmetic,
+        minmax,
+        _reorder,
+    ]
+
+    exhaustive = True
